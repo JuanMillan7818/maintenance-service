4adbec14967fc62e92086f1df7406fb896bef7c7
Juan Pablo <juan.holgin@correounivalle.edu.co>
2022-01-25 17:22:44 -0500

    Deploy

--- src/config-orm/config-orm.service.ts
+++ src/config-orm/config-orm.service.ts
@@ -5,12 +5,17 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export class ConfigOrmService {
     public getTypeOrmConfig(): TypeOrmModuleOptions {          
         return {
+            url: process.env.DATABASE_URL,
             type: 'postgres',
-            host: process.env.POSTGRES_HOST,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+            /*host: process.env.POSTGRES_HOST,
             port: parseInt(process.env.POSTGRES_PORT),
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
+            */
             entities: ['dist/**/*.entity{.ts,.js}'],
             //synchronize: true,           
             autoLoadEntities: true
